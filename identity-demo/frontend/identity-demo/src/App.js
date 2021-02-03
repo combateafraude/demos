7ba702e0c7d94d7d96f62ccb70ac7deb8dd7ce02
Bernardo Luis Hamester <37083276+bernardox0L@users.noreply.github.com>
2021-02-03 15:05:15 -0300

    feat(identity-demo): frontend integração

--- identity-demo/frontend/identity-demo/src/App.js
+++ identity-demo/frontend/identity-demo/src/App.js
@@ -1,3 +1,4 @@
+import React, { useState } from 'react'
 import './App.css';
 import axios from 'axios';
 import IdentitySdk from '@combateafraude/identity-sdk'
@@ -5,15 +6,16 @@ import IdentitySdk from '@combateafraude/identity-sdk'
 axios.defaults.baseURL = 'http://localhost:8081';
 
 function App() {
+  const [isAuth, setIsAuth] = useState(false)
   const signIn = async () => {
     const cpf = document.getElementById('cpf');
     const password = document.getElementById('password');
 
-    const { identityPolicyId, identityToken, mobileToken } = await axios.post('/credentials', { cpf: cpf.value, password: password.value });
-
+    const credentials = await axios.post('/credentials', { cpf: cpf.value, password: password.value });
+    const { identityPolicyId, identityToken, mobileToken } = credentials.data;
     const identityOptions = { mobileToken: mobileToken, throwOnRecall: true };
     const identity = new IdentitySdk(identityToken, identityOptions);
-    const response = await identity.verifyPolicy(cpf, identityPolicyId);
+    const response = await identity.verifyPolicy(cpf.value, identityPolicyId);
 
     if (identity.isSdkError(response)) {
       // Erro ao executar o SDK
@@ -25,6 +27,7 @@ function App() {
         // Usuário está autorizado
         // Enviar a attestation para seu backend e validá-la lá
         await axios.post('/login', { cpf: cpf.value, password: password.value, attestation: attestation });
+        setIsAuth(true)
       }
       else {
         // Usuário não está autorizado
@@ -34,12 +37,19 @@ function App() {
   }
   return (
     <div className="container">
+      Insira o CPF de uma identidade existente para testar a autenticação.
       <div className="content">
-        CPF:
+        <b>CPF:</b>
         <input id="cpf" className="input-login"></input>
-        Senha:
+        <b>Senha:</b>
         <input id="password" className="input-login" type="password"></input>
         <button onClick={signIn}>Entrar</button>
+        {isAuth ? <div className="div-label">
+          <span className="auth-label">Autorizado</span>
+        </div> : <div className="no-div-label">
+            <span className="no-auth-label">Não autorizado</span>
+          </div>}
+
       </div>
 
     </div>
